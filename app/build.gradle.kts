32023fce15f92a33b1d90fa5f0d1b59a492a2020
restoreback5-tech <restoreback5@gmail.com>
2026-03-31 01:01:20 -0600

    fix: lottie dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.airbnb.lottie:lottie:6.4.0")
-    implementation("com.airbnb.lottie:lottie:6.1.0")
+   
 }
